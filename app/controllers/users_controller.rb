74d09680d7f3d0c394574559d8e7f04c0a031df7
wrakc <carlosveiga1618@gmail.com>
2020-09-18 17:00:52 -0300

    update form to accept user hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+  
   def new
   end
 
   def create
+    @user = User.new(user_name: params[:user_name], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 end
