5a23610acd7148890eca9cc0120224d94fefbf79
Sanad <s_wj_88@hotmail.com>
2020-09-18 23:26:38 +0300

    Comment out the private method user_params and redo the User.new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_name: params[:user_name], email: params[:email], password: params[:password])
 
     if @user.save
       redirect_to new_user_path
@@ -12,10 +12,10 @@ class UsersController < ApplicationController
       render :new
     end
   end
-
+/
   private
   def user_params
     params.require(:user).permit(:user_name,:email,:password)
   end
-
+/
 end
